7213af9bf6bd8960a2995415e1d71aa13de9e868
yoona0428 <urinus0475@gmail.com>
2025-10-15 21:59:07 +0900

    Add Text File [Homework4.java]

---
+++ Homework4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Homework4 {
+
+    static int gcd(int m, int n) {
+        if (n == 0) { return m; }
+        return gcd(n, m % n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("두 수를 입력하세요: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int result1 = gcd(a, b);
+
+        System.out.printf("두 수의 최대공약수는 %s입니다.%n", result1);
+    }
+}
